da27603a4466f223a4de7f6962fd950d573d81d2
Meghan Flynn <mflynn333@gmail.com>
2020-01-21 01:56:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
-
+class CashRegister
+  
+  attr_accessor :discount, :total, :last_transaction
+  
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item (title, price, quantity = 1)
+    #price * quantity = total 
+    @total += price * quantity
+    quantity.times do 
+      items << title 
+    end 
+  end 
+  
+  def apply_discount 
+    if discount == 0 
+      "There is no discount to apply."
+    else 
+      x = (@total * (@discount.to_f / 100)).to_i
+      @total -= x
+      "After the discount, the total comes to $#{@total}."
+    end 
+  end 
+  
+  def items
+    @items
+  end 
+  
+  def void_last_transaction
+    @total = @total -= @last_transaction
+  end 
+  
+end 
